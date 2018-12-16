805c2fbbb3929b5d8994d0e86207b05076a837e8
Thibault Gérondal <iot@tycale.be>
2018-12-16 16:48:28 +0100

    Ability to manage multiple channels for a given device
    This commit includes multiples new features :
    * Ability to control multiple 'dps' values for a given device.
    * Ability to connect to a MQTT broker with username/password (given via env.
    variable)
    * Update of the dependency tuya-api
    
    I also cleaned a bit the code. I was not able to keep the history of my
    changes because I have changed the file format from DOS to Unix.
    
    Signed-off-by: Thibault Gérondal <iot@tycale.be>

--- tuya-color.js
+++ tuya-color.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const convert = require('color-convert');
 const debug = require('debug')('TuyaColor');
 
@@ -6,24 +8,24 @@ const debug = require('debug')('TuyaColor');
  */
 function TuyaColorLight() {
 
-    this.colorMode = 'white'; // or 'colour'
-    this.brightness = 100; // percentage value use _convertValToPercentage functions below.
+  this.colorMode = 'white'; // or 'colour'
+  this.brightness = 100; // percentage value use _convertValToPercentage functions below.
 
-    this.color = {
-        H: 130,
-        S: 100,
-        L: 50
-    };
+  this.color = {
+    H: 130,
+    S: 100,
+    L: 50,
+  };
 
-    this.hue = this.color.H;
-    this.saturation = this.color.S;
-    this.lightness = this.color.L;
+  this.hue = this.color.H;
+  this.saturation = this.color.S;
+  this.lightness = this.color.L;
 
-    this.colorTemperature = 255;
-    this.colorTempMin = 153;
-    this.colorTempMax = 500;
+  this.colorTemperature = 255;
+  this.colorTempMin = 153;
+  this.colorTempMax = 500;
 
-    this.dps = {};
+  this.dps = {};
 }
 
 /**
@@ -31,10 +33,10 @@ function TuyaColorLight() {
  * @param  {Integer} percentage 0-100 percentage value
  * @returns {Integer} color value from 0-255
  */
-TuyaColorLight.prototype._convertPercentageToVal = function (percentage) {
-    var tmp = Math.round(255 * (percentage / 100));
-    debug('Converted ' + percentage + ' to: ' + tmp);
-    return tmp;
+TuyaColorLight.prototype._convertPercentageToVal = function(percentage) {
+  var tmp = Math.round(255 * (percentage / 100));
+  debug('Converted ' + percentage + ' to: ' + tmp);
+  return tmp;
 };
 
 /**
@@ -42,10 +44,10 @@ TuyaColorLight.prototype._convertPercentageToVal = function (percentage) {
  * @param  {Integer} val 0-255 color value
  * @returns {Integer} HK-Value
  */
-TuyaColorLight.prototype._convertValToPercentage = function (val) {
-    var tmp = Math.round((val / 255) * 100);
-    debug('Converted ' + val + ' to: ' + tmp);
-    return tmp;
+TuyaColorLight.prototype._convertValToPercentage = function(val) {
+  var tmp = Math.round((val / 255) * 100);
+  debug('Converted ' + val + ' to: ' + tmp);
+  return tmp;
 };
 
 /**
@@ -53,21 +55,21 @@ TuyaColorLight.prototype._convertValToPercentage = function (val) {
  * @param  {Integer} val
  * @returns {Integer} percentage from 0-100
  */
-TuyaColorLight.prototype._convertColorTemperature = function (val) {
-    var tmpRange = this.colorTempMax - this.colorTempMin;
-    var tmpCalc = Math.round((val / this.colorTempMax) * 100);
+TuyaColorLight.prototype._convertColorTemperature = function(val) {
+  var tmpRange = this.colorTempMax - this.colorTempMin;
+  var tmpCalc = Math.round((val / this.colorTempMax) * 100);
 
-    debug('HK colorTemp Value: ' + val);
-    debug('HK colorTemp scale min : ' + this.colorTempMin);
-    debug('HK colorTemp scale max : ' + this.colorTempMax);
-    debug('HK colorTemp range (tmpRange): ' + tmpRange);
-    debug('HK colorTemp % tmpCalc: ' + tmpCalc);
+  debug('HK colorTemp Value: ' + val);
+  debug('HK colorTemp scale min : ' + this.colorTempMin);
+  debug('HK colorTemp scale max : ' + this.colorTempMax);
+  debug('HK colorTemp range (tmpRange): ' + tmpRange);
+  debug('HK colorTemp % tmpCalc: ' + tmpCalc);
 
-    var tuyaColorTemp = this._convertPercentageToVal(tmpCalc);
+  var tuyaColorTemp = this._convertPercentageToVal(tmpCalc);
 
-    debug('HK tuyaColorTemp: ' + tuyaColorTemp);
+  debug('HK tuyaColorTemp: ' + tuyaColorTemp);
 
-    return tuyaColorTemp;
+  return tuyaColorTemp;
 };
 
 /**
@@ -75,24 +77,24 @@ TuyaColorLight.prototype._convertColorTemperature = function (val) {
  * @param  {Integer} val
  * @returns {Integer} HK-Value
  */
-TuyaColorLight.prototype._convertColorTemperatureToHK = function (val) {
+TuyaColorLight.prototype._convertColorTemperatureToHK = function(val) {
 
-    var tuyaColorTempPercent = this._convertValToPercentage(this.colorTemperature);
-    var tmpRange = this.colorTempMax - this.colorTempMin;
-    var tmpCalc = Math.round((tmpRange * (tuyaColorTempPercent / 100)) + this.colorTempMin);
-    var hkValue = Math.round(tmpCalc);
+  var tuyaColorTempPercent = this._convertValToPercentage(this.colorTemperature);
+  var tmpRange = this.colorTempMax - this.colorTempMin;
+  var tmpCalc = Math.round((tmpRange * (tuyaColorTempPercent / 100)) + this.colorTempMin);
+  var hkValue = Math.round(tmpCalc);
 
-    debug('Tuya color Temperature : ' + val);
-    debug('Tuya color temp Percent of 255: ' + tuyaColorTempPercent + '%');
+  debug('Tuya color Temperature : ' + val);
+  debug('Tuya color temp Percent of 255: ' + tuyaColorTempPercent + '%');
 
-    debug('HK colorTemp scale min : ' + this.colorTempMin);
-    debug('HK colorTemp scale max : ' + this.colorTempMax);
+  debug('HK colorTemp scale min : ' + this.colorTempMin);
+  debug('HK colorTemp scale max : ' + this.colorTempMax);
 
-    debug('HK Color Temp Range: ' + tmpRange);
-    debug('HK range %: ' + tuyaColorTempPercent);
-    debug('HK Value: ' + hkValue);
+  debug('HK Color Temp Range: ' + tmpRange);
+  debug('HK range %: ' + tuyaColorTempPercent);
+  debug('HK Value: ' + hkValue);
 
-    return hkValue;
+  return hkValue;
 };
 
 /**
@@ -100,9 +102,9 @@ TuyaColorLight.prototype._convertColorTemperatureToHK = function (val) {
  * @param  {String} h
  * @returns {boolean}
  */
-TuyaColorLight.prototype._ValIsHex = function (h) {
-    debug("Check if value is hex", h);
-    return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(h)
+TuyaColorLight.prototype._ValIsHex = function(h) {
+  debug('Check if value is hex', h);
+  return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(h);
 };
 
 /**
@@ -110,63 +112,63 @@ TuyaColorLight.prototype._ValIsHex = function (h) {
  * @param  {Integer} brightness
  * @return {string} brightness as HEX value
  */
-TuyaColorLight.prototype._getAlphaHex = function (brightness) {
-    var i = brightness / 100;
-    var alpha = Math.round(i * 255);
-    var hex = (alpha + 0x10000).toString(16).substr(-2);
-    var perc = Math.round(i * 100);
+TuyaColorLight.prototype._getAlphaHex = function(brightness) {
+  var i = brightness / 100;
+  var alpha = Math.round(i * 255);
+  var hex = (alpha + 0x10000).toString(16).substr(-2);
+  var perc = Math.round(i * 100);
 
-    debug('alpha percent: ' + perc + '% hex: ' + hex + ' alpha: ' + alpha);
-    return hex;
+  debug('alpha percent: ' + perc + '% hex: ' + hex + ' alpha: ' + alpha);
+  return hex;
 };
 
 /**
  * Set saturation from value
  * @param  {Integer} value
  */
-TuyaColorLight.prototype.setSaturation = function (value) {
-    this.color.S = value;
-    this.saturation = value;
-    this.colorMode = 'colour';
+TuyaColorLight.prototype.setSaturation = function(value) {
+  this.color.S = value;
+  this.saturation = value;
+  this.colorMode = 'colour';
 
-    debug('SET SATURATION: ' + value);
+  debug('SET SATURATION: ' + value);
 };
 
 /**
  * Set Brightness
  * @param  {Integer} value
  */
-TuyaColorLight.prototype.setBrightness = function (value) {
-    this.brightness = value;
-    var newValue = this._convertPercentageToVal(value);
-    debug("BRIGHTNESS from UI: " + value + ' Converted from 100 to 255 scale: ' + newValue);
-}
+TuyaColorLight.prototype.setBrightness = function(value) {
+  this.brightness = value;
+  var newValue = this._convertPercentageToVal(value);
+  debug('BRIGHTNESS from UI: ' + value + ' Converted from 100 to 255 scale: ' + newValue);
+};
 
 /**
  * @param  {} value
  */
-TuyaColorLight.prototype.setHue = function (value) {
-    debug('SET HUE: ' + value);
-    debug('Saturation Value: ' + this.color.S);
-    this.color.H = value;
+TuyaColorLight.prototype.setHue = function(value) {
+  debug('SET HUE: ' + value);
+  debug('Saturation Value: ' + this.color.S);
+  this.color.H = value;
 
-    //check color and set colormode if necessary
-    debug("colormode", value, this.color.S);
-    if (value === 0 && this.color.S === 0) {
-        this.colorMode = 'white';
-        debug('SET Color Mode: \'white\'');
-    } else {
-        this.colorMode = 'colour';
-        debug('SET Color Mode: \'colour\' -- dahhhhhh british spelling \'coulour\' really is annoying... why you gotta be special?');
-    }
+  // check color and set colormode if necessary
+  debug('colormode', value, this.color.S);
+  if (value === 0 && this.color.S === 0) {
+    this.colorMode = 'white';
+    debug('SET Color Mode: \'white\'');
+  } else {
+    this.colorMode = 'colour';
+    debug('SET Color Mode: \'colour\' -- dahhhhhh british spelling \'coulour\' really is annoying... why you gotta be special?');
+  }
 
 
-    return {
-        color: this.color,
-        colorMode: this.colorMode,
-        hue: this.color.H,
-        saturation: this.saturation
-    };
+  return {
+    color: this.color,
+    colorMode: this.colorMode,
+    hue: this.color.H,
+    saturation: this.saturation,
+  };
 };
 
 /**
@@ -175,73 +177,73 @@ TuyaColorLight.prototype.setHue = function (value) {
  * @param  {Integer} saturation
  * @param  {Integer} brightness
  */
-TuyaColorLight.prototype.setHSL = function (hue, saturation, brightness) {
-    this.setSaturation(saturation);
-    this.setBrightness(brightness);
-    this.setHue(hue);
-}
+TuyaColorLight.prototype.setHSL = function(hue, saturation, brightness) {
+  this.setSaturation(saturation);
+  this.setBrightness(brightness);
+  this.setHue(hue);
+};
 
 /**
  * Set color from given string
  * @param  {String} colorValue could be HEX or HSL color type
  * @returns {Object} dps settings for given color
  */
-TuyaColorLight.prototype.setColor = function (colorValue) {
-    debug("Recieved color", colorValue);
+TuyaColorLight.prototype.setColor = function(colorValue) {
+  debug('Recieved color', colorValue);
 
-    if (this._ValIsHex(colorValue)) {
-        debug("Color is Hex");
-        var color = convert.hex.hsl(colorValue);
-    } else {
-        debug("Color is HSL");
-        var color = colorValue.split(",");
-        // convert strings to numbers
-        color.forEach(function (element, key) {
-            color[key] = parseInt(element, 10);
-        });
-    }
-    debug("Converted color as HSL", {
-        0: color[0] + " - " + typeof color[0],
-        1: color[1] + " - " + typeof color[1],
-        2: color[2] + " - " + typeof color[2]
-    })
+  if (this._ValIsHex(colorValue)) {
+    debug('Color is Hex');
+    var color = convert.hex.hsl(colorValue);
+  } else {
+    debug('Color is HSL');
+    var color = colorValue.split(',');
+    // convert strings to numbers
+    color.forEach(function(element, key) {
+      color[key] = parseInt(element, 10);
+    });
+  }
+  debug('Converted color as HSL', {
+    0: color[0] + ' - ' + typeof color[0],
+    1: color[1] + ' - ' + typeof color[1],
+    2: color[2] + ' - ' + typeof color[2],
+  });
 
-    this.setHSL(color[0], color[1], color[2]);
-    return this.getDps();
-}
+  this.setHSL(color[0], color[1], color[2]);
+  return this.getDps();
+};
 
 /**
  * get dps settings for current color
  * @returns {Object} dps settings
  */
-TuyaColorLight.prototype.getDps = function () {
-    var color = this.color;
+TuyaColorLight.prototype.getDps = function() {
+  var color = this.color;
 
-    var lightness = Math.round(this.brightness / 2);
-    var brightness = this.brightness;
-    var apiBrightness = this._convertPercentageToVal(brightness);
-    var alphaBrightness = this._getAlphaHex(brightness);
+  var lightness = Math.round(this.brightness / 2);
+  var brightness = this.brightness;
+  var apiBrightness = this._convertPercentageToVal(brightness);
+  var alphaBrightness = this._getAlphaHex(brightness);
 
-    var hexColor1 = convert.hsl.hex(color.H, color.S, lightness);
+  var hexColor1 = convert.hsl.hex(color.H, color.S, lightness);
 
-    var hexColor2 = convert.hsl.hex(0, 0, lightness);
+  var hexColor2 = convert.hsl.hex(0, 0, lightness);
 
-    var colorTemperature = this.colorTemperature;
+  var colorTemperature = this.colorTemperature;
 
-    var lightColor = (hexColor1 + hexColor2 + alphaBrightness).toLowerCase();
+  var lightColor = (hexColor1 + hexColor2 + alphaBrightness).toLowerCase();
 
-    var temperature = (this.colorMode === 'colour') ? 255 : this._convertColorTemperature(colorTemperature);
+  var temperature = (this.colorMode === 'colour') ? 255 : this._convertColorTemperature(colorTemperature);
 
-    dpsTmp = {
-        '1': true,
-        '2': this.colorMode,
-        '3': apiBrightness,
-        '4': temperature,
-        '5': lightColor
-        // '6' : hexColor + hexColor + 'ff'
-    };
-    debug("dps", dpsTmp);
-    return dpsTmp;
-}
+  dpsTmp = {
+    1: true,
+    2: this.colorMode,
+    3: apiBrightness,
+    4: temperature,
+    5: lightColor,
+    // '6' : hexColor + hexColor + 'ff'
+  };
+  debug('dps', dpsTmp);
+  return dpsTmp;
+};
 
 module.exports = TuyaColorLight;
--- tuya-mqtt.js
+++ tuya-mqtt.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const mqtt = require('mqtt');
 const TuyaDevice = require('./tuya-device');
 const debug = require('debug')('tuya-mqtt');
@@ -5,81 +7,111 @@ const debugMqtt = require('debug')('mqtt');
 var cleanup = require('./cleanup').Cleanup(onExit);
 
 function bmap(istate) {
-    return istate ? 'ON' : "OFF";
-}
-
-function bmap(istate) {
-    return istate ? 'ON' : "OFF";
+  return istate.toString() == 'true' ? 'ON' : 'OFF';
 }
 
 var connected = undefined;
 
 const CONFIG = {
-    host: 'localhost',
-    port: 1883,
-    topic: "tuya/"
+  host: 'localhost',
+  port: 1883,
+  topic: 'tuya/',
+};
+
+const MQTT_OPTS = {
+  retain: true,
+  qos: 2,
+};
+
+var config_mqtt = {
+  host: CONFIG.host,
+  port: CONFIG.port,
+};
+
+if (process.env.MQTT_USER) {
+  config_mqtt.username = process.env.MQTT_USER;
 }
 
-const mqtt_client = mqtt.connect({
-    host: CONFIG.host,
-    port: CONFIG.port
+if (process.env.MQTT_PASS) {
+  config_mqtt.password = process.env.MQTT_PASS;
+}
+
+const mqtt_client = mqtt.connect(config_mqtt);
+
+mqtt_client.on('connect', function(err) {
+  debugMqtt('Connection established with MQTT server');
+  connected = true;
+  var topic = CONFIG.topic + '#';
+  mqtt_client.subscribe(topic);
 });
 
-mqtt_client.on('connect', function (err) {
-    debugMqtt("Verbindung mit MQTT-Server hergestellt");
-    connected = true;
-    var topic = CONFIG.topic + '#';
-    mqtt_client.subscribe(topic);
+mqtt_client.on('reconnect', function(error) {
+  if (connected) {
+    debugMqtt('Connection with MQTT server was interrupted. Renewed connection.');
+  } else {
+    debugMqtt('Unable to connect to MQTT server.');
+  }
+  connected = false;
 });
 
-mqtt_client.on("reconnect", function (error) {
-    if (connected) {
-        debugMqtt("Verbindung mit MQTT-Server wurde unterbrochen. Erneuter Verbindungsversuch!");
-    } else {
-        debugMqtt("Verbindung mit MQTT-Server konnte nicht herrgestellt werden.");
-    }
-    connected = false;
-});
-
-mqtt_client.on("error", function (error) {
-    debugMqtt("Verbindung mit MQTT-Server konnte nicht herrgestellt werden.", error);
-    connected = false;
+mqtt_client.on('error', function(error) {
+  debugMqtt('Unable to connect to MQTT server', error);
+  connected = false;
 });
 
 /**
  * execute function on topic message
  */
-mqtt_client.on('message', function (topic, message) {
-    try {
-        message = message.toString();
-        message = message.toLowerCase();
-        var topic = topic.split("/");
-        var options = {
-            type: topic[1],
-            id: topic[2],
-            key: topic[3],
-            ip: topic[4],
-        };
-        var exec = topic[5];
+mqtt_client.on('message', function(topic, message, packet) {
+  
+  if (packet.retain && topic.includes('command')) return;
 
-        if (options.type == "socket" || options.type == "lightbulb") {
-            debug("device", options);
-            debug("message", message);
-            var device = new TuyaDevice(options);
+  try {
+    message = message.toString();
+    message = message.toLowerCase();
+    var topic = topic.split('/');
+    var options = {
+      type: topic[1],
+      id: topic[2],
+      key: topic[3],
+      ip: topic[4],
+    };
+    var exec = topic[6];
 
-            if (exec == "command") {
-                var status = topic[6];
-                device.onoff(status);
-            }
-            if (exec == "color") {
-                var color = message;
-                device.setColor(color);
-            }
-        }
-    } catch (e) {
-        debug(e);
+    if (options.type == 'socket' || options.type == 'lightbulb') {
+      var device = new TuyaDevice(options);
+
+      if (exec == 'command') {
+        var status = topic[7];
+        var dps_to_change = topic[5];
+        debug('mqtt command', status, dps_to_change);
+        device.onoff(status, dps_to_change);
+      }
+      if (exec == 'color') {
+        var color = message;
+        device.setColor(color);
+      }
     }
+  } catch (e) {
+    debug(e);
+  }
 });
+
+/**
+ * Publish current TuyaDevice socket device state to MQTT-Topic
+ * @param {topic} topic
+ * @param {dps} pds ID to parse
+ * @param {value} value of pds ID
+ */
+function sendParseSocketStatus(topic, dps, value) {
+  if(dps <= 2) { // status of switches
+    value = bmap(value);
+  } else { // consumption etc.
+    value = value.toString();
+  }
+  mqtt_client.publish(topic, value, MQTT_OPTS);
+  debug('mqtt switch status updated to:' + topic + ' -> ' + value);
+}
 
 /**
  * Publish current TuyaDevice state to MQTT-Topic
@@ -87,72 +119,78 @@ mqtt_client.on('message', function (topic, message) {
  * @param {boolean} status
  */
 function publishStatus(device, status) {
-    if (mqtt_client.connected == true) {
-        try {
-            var type = device.type;
-            var tuyaID = device.options.id;
-            var tuyaKey = device.options.key;
-            var tuyaIP = device.options.ip;
+  if (mqtt_client.connected == true) {
+    for (var dps in status){
+      try {
+        var type = device.type;
+        var tuyaID = device.options.id;
+        var tuyaKey = device.options.key;
+        var tuyaIP = device.options.ip;
+        var tuyaDps = dps;
 
-            if (tuyaID != undefined && tuyaKey != undefined && tuyaIP != undefined) {
-                var topic = "tuya/" + type + "/" + tuyaID + "/" + tuyaKey + "/" + tuyaIP + "/state";
-                mqtt_client.publish(topic, status, {
-                    retain: true,
-                    qos: 2
-                });
-                debug("mqtt status updated to:" + topic + " -> " + status);
-            } else {
-                debug("mqtt status not updated");
-            }
-        } catch (e) {
-            debug(e);
+        if (tuyaID != undefined && tuyaKey != undefined && tuyaIP != undefined) {
+          var topic = CONFIG.topic + type + '/' + tuyaID + '/' + tuyaKey + '/' + tuyaIP + '/' + tuyaDps + '/state';
+          var value = status[dps];
+          if (type == 'socket') {
+            sendParseSocketStatus(topic, dps, value);
+          } else {
+            value = bmap(value);
+            mqtt_client.publish(topic, value, MQTT_OPTS);
+            debug('mqtt status updated to:' + topic + ' -> ' + value);
+          }
+        } else {
+          debug('mqtt status not updated');
         }
+      } catch (e) {
+        debug(e);
+      }
     }
+  }
 }
 
 /**
  * event fires if TuyaDevice sends data
  * @see TuyAPI (https://github.com/codetheweb/tuyapi)
  */
-TuyaDevice.onAll('data', function (data) {
-    debug('Data from device ' + this.type + ' :', data);
-    var status = data.dps['1'];
-    if (this.type == "lightbulb" && status == undefined) {
-        status = true;
-    }
-    publishStatus(this, bmap(status));
+TuyaDevice.onAll('data', function(data) {
+  var status = null;
+  status = data.dps;
+  if (this.type == 'lightbulb' && status == undefined) {
+    status = true;
+  }
+  publishStatus(this, status);
 });
 
 /**
  * MQTT connection tester
  */
 function MQTT_Tester() {
-    this.interval = null;
+  this.interval = null;
 
-    function mqttConnectionTest() {
-        if (mqtt_client.connected != connected) {
-            connected = mqtt_client.connected;
-            if (connected) {
-                debugMqtt('MQTT-Server verbunden.');
-            } else {
-                debugMqtt('MQTT-Server nicht verbunden.');
-            }
-        }
+  function mqttConnectionTest() {
+    if (mqtt_client.connected != connected) {
+      connected = mqtt_client.connected;
+      if (connected) {
+        debugMqtt('Connected to the MQTT server.');
+      } else {
+        debugMqtt('Not connected to the MQTT server.');
+      }
     }
+  }
 
-    this.destroy = function () {
-        clearInterval(this.interval);
-        this.interval = undefined;
-    }
+  this.destroy = function() {
+    clearInterval(this.interval);
+    this.interval = undefined;
+  };
 
-    this.connect = function () {
-        this.interval = setInterval(mqttConnectionTest, 1500);
-        mqttConnectionTest();
-    }
+  this.connect = function() {
+    this.interval = setInterval(mqttConnectionTest, 1500);
+    mqttConnectionTest();
+  };
 
-    var constructor = (function (that) {
-        that.connect.call(that);
-    })(this);
+  var constructor = (function(that) {
+    that.connect.call(that);
+  })(this);
 }
 var tester = new MQTT_Tester();
 
@@ -160,6 +198,6 @@ var tester = new MQTT_Tester();
  * Function call on script exit
  */
 function onExit() {
-    TuyaDevice.disconnectAll();
-    tester.destroy();
+  TuyaDevice.disconnectAll();
+  tester.destroy();
 };
